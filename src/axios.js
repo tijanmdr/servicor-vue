a81885b2f94656764ca2769441f72023b1d6004c
tijan <tijanmdR@gmail.com>
2024-04-11 21:02:22 +1000

    incorrect token fix

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { userStore } from '@/stores/index.js';
+import {userStore} from '@/stores/index.js';
+import {onBeforeMount} from "vue";
 
 const axiosClient = axios.create({
     baseURL: 'http://localhost:8000/api/'
@@ -9,6 +10,15 @@ axiosClient.interceptors.request.use((config) => {
     const _store = userStore();
     config.headers.Authorization = 'Bearer ' + _store.token;
     return config;
+});
+
+axiosClient.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    const _store = userStore();
+    if (error.response.status === 401) {
+        _store.logout();
+    }
 });
 
 export default axiosClient;
--- src/components/partials/Menu.vue
+++ src/components/partials/Menu.vue
@@ -16,7 +16,7 @@ function searchText() {
 <template>
   <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <RouterLink to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <img
             src="https://flowbite.com/docs/images/logo.svg"
             class="h-8"
@@ -25,7 +25,7 @@ function searchText() {
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
         >Servicor</span
         >
-      </a>
+      </RouterLink>
       <div class="flex md:order-1">
         <button
             v-if="store.token"
@@ -33,22 +33,19 @@ function searchText() {
             data-collapse-toggle="navbar-search"
             aria-controls="navbar-search"
             aria-expanded="false"
-            class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
-        >
+            class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
           <svg
               class="w-5 h-5"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
-              viewBox="0 0 20 20"
-          >
+              viewBox="0 0 20 20">
             <path
                 stroke="currentColor"
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-            />
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
           </svg>
           <span class="sr-only">Search</span>
         </button>
